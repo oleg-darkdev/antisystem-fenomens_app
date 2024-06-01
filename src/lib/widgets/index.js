d651538874a66b04b02cb22ca2ffe479e86f6cff
DarkDev <darkdev_brand@riseup.net>
2024-06-02 02:18:02 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -1,10 +1,9 @@
 import BoardgameSection from './ui/landing/BoardgameSection.svelte';
-import Hero from './ui/landing/Hero.svelte';
-import SupportUs from './ui/landing/SupportUs.svelte';
+// import Hero from './ui/landing/Hero.svelte';
+// import SupportUs from './ui/landing/SupportUs.svelte';
 import Footer from './ui/landing/Footer.svelte';
-import AntiSystems from './ui/landing/AntiSystems.svelte';
+// import AntiSystems from './ui/landing/AntiSystems.svelte';
 // import OurClients from './ui/landing/OurClients.svelte';
 
-export { BoardgameSection, Hero, AntiSystems, SupportUs, Footer };
+export { BoardgameSection,  Footer };
 
-export { BoardgameSection, Hero, Aside, AntiSystems, SupportUs, AnimaterdLogo, OurClients, Footer };
