284ba3c4ae93018b8cc8bf93601a5b0b8aaef8b9
DarkDev <darkdev_brand@riseup.net>
2024-06-12 18:17:20 +0300

    upd text layout & fix empty link logic

--- src/lib/widgets/ui/landing/BoardgameSection.svelte
+++ src/lib/widgets/ui/landing/BoardgameSection.svelte
@@ -6,7 +6,7 @@
 
 <section class="section max-h-screen h-screen">
 	<header class="section__header" style='background-color: {boardgame.brandColor}'>
-		<h3 class="section__title text-black">{boardgame.title}</h3>
+		<h3 class="section__title max-w-[200px] text-black">{boardgame.title}</h3>
 	</header>
 	<div  class="section__content " data-content>
 
@@ -18,12 +18,14 @@
 			alt="Parasaurolophus"
 		/>
 
-		<h2 class="section__main-title section__main-title--left max-w-xl">{boardgame.title}</h2>
+		<h2 class="section__main-title section__main-title--left max-w-lg  font-bebasNeue ">{boardgame.title}</h2>
 		<div class="section__intro gentle-giants__intro">
 			<p class='mb-6'>
 				{boardgame.shortDesc}
 			</p>
-			<p><a href="/" class='text-white'>Find out more</a></p>
+			<p>
+        <a target='_blank' class:hidden={boardgame.links.site.length < 2} href="{boardgame.links.site}" class='text-white'>Find out more</a>
+      </p>
 		</div>
 	</div>
 </section>
