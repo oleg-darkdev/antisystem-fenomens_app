496449ebbedee0c28c276976fea75e2e6438a54e
DarkDev <darkdev_brand@riseup.net>
2024-07-03 22:02:54 +0300

    refactoring to components

---
+++ src/lib/widgets/ui/landing/Api.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { antISystems } from '$lib/shared';
+</script>
+
+<section class="section max-h-screen w-full bg-black h-screen">
+	<!-- <header class="section__header__about bg-black" style=''>
+		<h3 class="section__title text-black">Public API</h3>
+	</header> -->
+	<div  class="section__content__about " data-content>
+		<div style="max-width: 100ch;" class="px-2 section__intro__about">
+		<h2 class="mb-8 section__main-title font-bebasNeue section__main-title--left max-w-xl">Public API</h2>
+		<div class='mb-4'>
+			<!-- <h3 class="mb-2 section__main-title section__main-title--left max-w-xl">OBJECTIVE</h3> -->
+			<p class='lg:text-left xl:text-left md:text-left text-center'>
+				To showcase the impact of anti-system movements on contemporary society and introduce you to unique forms of countercultural practices in an interactive and educational format.
+			</p>
+
+
+		</div>
+
+			<a name='api' />
+
+
+			{#each antISystems as boardgame}
+				<p class='mx-auto flex flex-row  my-2 '>
+					<img src='/images/logos/{boardgame.logo}' class='lg:h-12 md:h-12 h-8 lg:mr-6 md:mr-4 mr-2 w-auto' alt='Boardgame {boardgame.title} logo'/>
+					<a class='lg:text-4xl md:text-4xl  text-lg text-black lg:px-6 md:px-4 px-2 lg:py-2 md:py-2 py-1 rounded-md cuttedText' style='background-color: {boardgame.brandColor}'  href="{boardgame.api}">{boardgame.api.substring(30, boardgame.api.length)}</a></p>
+			{/each}
+		</div>
+	</div>
+</section>
--- src/lib/widgets/ui/landing/Footer.svelte
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -1,8 +1,42 @@
-<script>
 
-</script>
+<footer class="section max-h-screen w-full bg-black h-screen">
+   <div  class="section__content__about " data-content>
+		<div style="max-width: 100ch;" class="px-2 section__intro__about">
+		<h2 class="mb-8 font-bebasNeue max-w-xl text-center">Contact</h2>
+		<div class='mb-4'>
+			<!-- <h3 class="mb-2 section__main-title section__main-title--left max-w-xl">OBJECTIVE</h3> -->
+      <div class=' max-w-2xl mx-auto my-auto'>
+          <div class='w-full max-w-md mb-4 mx-auto'>
+            <p class=' text-center'>This is an open-source project, with <a class='font-bold' href="" target='_blank'>open repository on github </a> and is distributed under a license <a class='font-bold' href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target='_blank'>CC BY-NC-SA</a>.</p>
+          </div>
 
-
-<footer class='bg-stone-900 h-[450px]'>
-
+          <div class='w-full  lg:mb-4 xl:mb-4 md:mb-4 mb-6  '>
+            <p class='text-center '>Idea Author: <a class='font-bold' href="https://oleg-darkdev.vercel.app/" target='_blank'>Oleg DarkDEV</a></p>
+          </div>
+          <div class='w-full mb-6 flex flex-wrap flex-row lg:justify-between xl:justify-between md:justify-between justify-center mx-auto'>
+              <div class='hover:scale-110 hover:transition hover:duration-1000  hover:ease-in-out'>
+                <a class='' href="https://darkdev-foundation.vercel.app/" target='_blank'>
+                <figure class=''>
+                  <figcaption class='text-sm '>Development by DarkDev.Foundation</figcaption>
+                  <img src="images/logos/darkdev.svg" class='h-16 transform-none scale-0' alt="DarkDEV logo" />
+                </figure>
+                </a>
+              </div>
+              <div class='w-[60px] xl:flex lg:flex md:flex hidden '>
+                  <div class='w-1 mx-auto bg-neutral-200 h-14 my-auto mx-auto'>
+                  </div>
+                </div>
+              <div class='mx-auto hover:scale-110 hover:transition lg:mt-0 xl:mt-0 md:mt-0 mt-4 hover:duration-1000  hover:ease-in-out'>
+                <a class='' href="https://44games.vercel.app/" target='_blank'>
+                <figure class=''>
+                  <figcaption class='text-sm mb-1 text-center'>Production by 44Games</figcaption>
+                  <img src="images/logos/44games.svg" class='h-16 transform-none scale-0' alt="44Games logo" />
+                </figure>
+                </a>
+              </div>
+          </div>
+        </div>
+  	  </div>
+		</div>
+	</div>
 </footer>
